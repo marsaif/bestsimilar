7039277648b0cef368483dec91f2ccaaafd20f38
= <=>
2022-06-26 15:23:57 +0100

    update

--- Scraper.py
+++ Scraper.py
@@ -1,5 +1,8 @@
+import json
 from time import sleep
 from typing import List
+from bs4 import BeautifulSoup
+import requests
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
@@ -9,17 +12,18 @@ from selenium.webdriver.chrome.options import Options
 class Scraper :
 
     def __init__(self) -> None:
-        self.counter = 2
+        self.counter = 1
 
         options = Options()
         options.headless = True
         options.add_argument('--no-sandbox')
         options.add_argument('--disable-dev-shm-usage')
-        self.driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()),options=options)
-        self.driver.maximize_window()
+        #self.driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()),options=options)
+        #self.driver.maximize_window()
 
-    def get_movie_info(self) -> List[dict]:
+    def get_movie_info(self,soup : BeautifulSoup) -> List[dict]:
         info = {
+            'title' : '',
             'Original name' : '' , 
             'Genre' : '' , 
             'Country' : '' , 
@@ -32,21 +36,24 @@ class Scraper :
             'image_url' : ''
             }
 
-        div_content = self.driver.find_element(By.CLASS_NAME,"column-content-c")
-        attrs = div_content.find_elements(By.CLASS_NAME,"attr")
+        div_content = soup.select_one(".column-content-c")
+        attrs = div_content.select(".attr")
         for attr in attrs:
-            type = attr.find_element(By.CLASS_NAME,"entry").text[:-1]
-            data = attr.find_element(By.CLASS_NAME,"value").text
+            type = attr.select_one(".entry").getText()[:-1]
+            data = attr.select_one(".value").getText().strip()
             info[type] = data
 
-        img = self.driver.find_element(By.CLASS_NAME,"img-responsive")
-        info['image_url'] = img.get_attribute("src")
-        print(info)
+        img = soup.select_one(".img-responsive")
+        info['image_url'] = img['src']
+        print(json.dumps(info, indent=2, default=str))
 
 
     def get_movies(self) -> None :
         url = f"https://bestsimilar.com/movies/{self.counter}"
-        self.driver.get(url)
-        self.get_movie_info()
+        headers = {'User-Agent': ''}
+
+        response = requests.get(url,headers=headers)
+        soup = BeautifulSoup(response.content, "html.parser")
+        self.get_movie_info(soup)
     
     
